9b7ad05143d395cb7265abc289a292fffb6d7c3c
orszaczkyanna <orszaczky.anna@gmail.com>
2024-07-29 22:03:52 +0200

    Test fetching weather data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
+import getWeatherData from "./services/weatherService";
 
 const App = () => {
+  const getWeather = async () => {
+    const data = await getWeatherData("weather", { q: "berlin" });
+    console.log(data);
+  };
+  getWeather();
+
   return (
     <div
       className="mx-auto max-w-screen-lg mt-4 py-5 px-32
---
+++ src/services/weatherService.js
@@ -0,0 +1,15 @@
+// https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.error(err));
+};
+
+export default getWeatherData;
