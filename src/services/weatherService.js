d01d355248fe9986c6cf5b30701a8b90e7c1519e
orszaczkyanna <orszaczky.anna@gmail.com>
2024-07-30 20:47:28 +0200

    Add comments to weatherService.js for fetching and formatting weather data

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -20,9 +20,9 @@ const iconUrlFromCode = (icon) =>
 
 // Convert Unix timestamp to formatted local time
 const formatToLocalTime = (
-  secs,
-  offset,
-  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+  secs, // alap Unix időbélyeg másodpercekben (dt)
+  offset, // időzóna eltolódása másodpercekben (timezone)
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a" // (default fomrat) e.g., Tuesday, 14 May 2024 | Local time: 07:21 AM
 ) => DateTime.fromSeconds(secs + offset, { zone: "utc" }).toFormat(format);
 
 // Destructure and format weather data from API response
@@ -31,15 +31,15 @@ const formatCurrent = (data) => {
     coord: { lat, lon },
     main: { temp, feels_like, temp_min, temp_max, humidity },
     name,
-    dt,
+    dt, // current datetime, when the data was fetched (in epoch time) (in seconds)
     sys: { country, sunrise, sunset },
-    weather,
+    weather, // array
     wind: { speed },
     timezone,
   } = data;
 
-  const { main: details, icon } = weather[0];
-  const formattedLocalTime = formatToLocalTime(dt, timezone);
+  const { main: details, icon } = weather[0]; // main: details <- rename "main" to "details"
+  const formattedLocalTime = formatToLocalTime(dt, timezone); // format = default fomrat
 
   return {
     lat,
@@ -51,12 +51,12 @@ const formatCurrent = (data) => {
     humidity,
     name,
     country,
-    sunrise: formatToLocalTime(sunrise, timezone, "hh:mm a"),
-    sunset: formatToLocalTime(sunset, timezone, "hh:mm a"),
+    sunrise: formatToLocalTime(sunrise, timezone, "hh:mm a"), // e.g, 05:33 AM
+    sunset: formatToLocalTime(sunset, timezone, "hh:mm a"), // e.g, 08:33 PM
     speed,
     details,
     icon: iconUrlFromCode(icon),
-    formattedLocalTime,
+    formattedLocalTime, // e.g., Tuesday, 14 May 2024 | Local time: 07:21 AM
     dt,
     timezone,
   };
@@ -68,8 +68,10 @@ const getFormattedWeatherData = async (searchParams) => {
     "weather",
     searchParams
   ).then(formatCurrent);
+  // ).then((data) => formatCurrent(data));
 
-  return { ...formattedCurrentWeather };
+  return { ...formattedCurrentWeather }; // return the formatted weather data object
+  // e.g., {lat: 52.5244, lon: 13.4105, temp: 299.33, feels_like: 299.33, temp_min: 298.14, …}
 };
 
 export default getFormattedWeatherData;
